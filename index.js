97458adeb4defb3122385ca0edb5a62dcb8e85dd
kaiumuddin <kaium.uddin2909@gmail.com>
2022-12-22 15:00:57 +0600

    finished

--- index.js
+++ index.js
@@ -29,32 +29,27 @@ async function run() {
 
         app.post('/enrole', async (req, res) => {
             const newCourse = req.body;
-            console.log(newCourse);
             const fromDb = await enroledCourseCollection.insertOne(newCourse);
             res.send(fromDb);
         });
 
         app.post('/addcourse', async (req, res) => {
             const newCourse = req.body;
-            console.log(newCourse);
             const fromDb = await courseCollection.insertOne(newCourse);
             res.send(fromDb);
         });
 
         app.post('/videos', async (req, res) => {
             const newVideo = req.body;
-            console.log(newVideo);
             const fromDb = await videoCollection.insertOne(newVideo);
             res.send(fromDb);
         });
 
         app.get('/mycourses/:email', async (req, res) => {
             const email = req.params.email;
-            console.log(email);
             const query = {email: email};
             const cursor = enroledCourseCollection.find(query);
             const enroledCourses = await cursor.toArray();
-            console.log(enroledCourses);
             res.send(enroledCourses);
         });
 
@@ -67,20 +62,16 @@ async function run() {
 
         app.get('/courses/:id', async (req, res) => {
             const id = req.params.id;
-            console.log(id);
             const query = {_id: ObjectId(id)};
             const course = await courseCollection.findOne(query);
-            console.log(course);
             res.send(course);
         });
 
         app.get('/courselessons/:id', async (req, res) => {
             const id = req.params.id;
-            console.log(id);
             const query = {courseid: id};
             const cursor = videoCollection.find(query);
             const videos = await cursor.toArray();
-            console.log(videos);
             res.send(videos);
         });
 
